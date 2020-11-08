a34c4c7b09c709fc607cccc3ef5f4b24efdf8a37
dma <damian.antoniak@hotmail.com>
2020-11-08 12:11:54 +0100

    onDestroy

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit, OnDestroy{
   constructor() {
     console.log('constructor()');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit()');
+  }
+
+  ngOnDestroy(): void {
+    console.log('onDistroy()');
   }
 }
